6b532c432c31352bb92b0d99572af31cf29af82c
jebakumar <sjebakumar3@gmail.com>
2025-11-09 13:24:16 +0530

    Style changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,33 @@
- document.querySelectorAll('.faq-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const answer = btn.nextElementSibling;
-      const icon = btn.querySelector('i');
+//  document.querySelectorAll('.faq-btn').forEach(btn => {
+//     btn.addEventListener('click', () => {
+//       const answer = btn.nextElementSibling;
+//       const icon = btn.querySelector('i');
       
-      answer.classList.toggle('hidden');
-      icon.classList.toggle('ri-add-line');
-      icon.classList.toggle('ri-subtract-line');
+//       answer.classList.toggle('hidden');
+//       icon.classList.toggle('ri-add-line');
+//       icon.classList.toggle('ri-subtract-line');
+//     });
+//   });
+ console.log('menuBtn');
+  const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+  let isOpen = false;
+  console.log('menuBtn');
+
+  menuBtn.addEventListener('click', () => {
+    isOpen = !isOpen;
+    mobileMenu.classList.toggle('hidden');
+    menuBtn.innerHTML = isOpen 
+      ? '<i class="ri-close-line text-2xl"></i>' 
+      : '<i class="ri-menu-line text-2xl"></i>';
+      console.log('clicked');
+  });
+
+  // Optional: close menu when clicking a link
+  document.querySelectorAll('#mobile-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.add('hidden');
+      menuBtn.innerHTML = '<i class="ri-menu-line text-2xl"></i>';
+      isOpen = false;
     });
   });
